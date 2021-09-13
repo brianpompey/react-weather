1d0cb00a2aa5a749ce27086dbc035ee09e822f01
brianpompey <brianthescholar@gmail.com>
2021-09-13 00:14:24 -0400

    Added search

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
