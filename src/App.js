99ea18e4812408d5d65477994fcabc712ac8ee4e
brianpompey <brianthescholar@gmail.com>
2021-09-13 00:00:32 -0400

    ADded search bar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
     <div className="app">
       <main>
         <div className="search-box">
-          
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+          />
         </div>
       </main>
     </div>
