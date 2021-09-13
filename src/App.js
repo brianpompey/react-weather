724eb4795449526e86230acebc1749bffc2ec460
brianpompey <brianthescholar@gmail.com>
2021-09-12 23:56:02 -0400

    Adding api key

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+const api = {
+  key: "2a680c7a58c281fbfcd8d9a2a5dcb921",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
     <div className="App">
-      
+      HEllo
     </div>
   );
 }
